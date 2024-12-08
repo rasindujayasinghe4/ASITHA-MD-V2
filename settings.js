11fab48b8524b12bac5dc052ad1267cb056f1b8d
rasindujayasinghe4 <rasindujayasinghe547@gmail.com>
2024-12-08 09:53:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Oz5jTaBL#IzlJYze2sOU96Vya4utSA9taBF5A0QxqbcYPPNCDkxE' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CypBVLxS#D5jgysdMVKEJkYEQUkXLsy9fUYnwYBI1MJSiiKrSF5Q' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
